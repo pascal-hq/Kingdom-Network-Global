3c4b6ca5a05f547b841c0cf017e2a037290502c0
MuthamaPK <pascalmuthama01@gmail.com>
2026-06-21 11:57:57 +0300

    Minor

--- back-end/kingdom_backend/settings.py
+++ back-end/kingdom_backend/settings.py
@@ -56,12 +56,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'kingdom_backend.wsgi.application'
 
 # Database - Use PostgreSQL on Render, SQLite locally
-DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
+DATABASE_URL = os.getenv('DATABASE_URL')
 DATABASES = {
     'default': dj_database_url.config(
         default=DATABASE_URL,
         conn_max_age=600,
-        ssl_require=True if not DEBUG else False
+        ssl_require=True
     )
 }
 
