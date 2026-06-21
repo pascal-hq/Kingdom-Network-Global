521cceec7ad26d8182f4919962d5a7e4bcfbef02
MuthamaPK <pascalmuthama01@gmail.com>
2026-06-21 12:35:34 +0300

    almost

--- back-end/build.sh
+++ back-end/build.sh
@@ -7,5 +7,3 @@ apt-get update && apt-get install -y libpq-dev gcc
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-python create_superuser.py
-python seed_data.py
